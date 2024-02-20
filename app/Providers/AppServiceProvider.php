1bec5676f0b7a770f24454a579ac315a68535c1b
TonyAndr <blueberry.acc@gmail.com>
2024-02-20 19:34:23 +0000

    Disabling https rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(env('APP_ENV', 'production') == 'production') { // use https only if env is production
-            URL::forceScheme('https');
-        }
+        // if(env('APP_ENV', 'production') == 'production') { // use https only if env is production
+        //     URL::forceScheme('https');
+        // }
     }
 }
