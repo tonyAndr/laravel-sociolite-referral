b3633a28b788e8d7b789a9ec9b62fe6a875638ec
TonyAndr <blueberry.acc@gmail.com>
2024-07-25 11:55:20 +0000

    Steps upd

--- resources/views/giveaway/step1.blade.php
+++ resources/views/giveaway/step1.blade.php
@@ -3,15 +3,17 @@
         <div
             class="flex flex-col py-10 px-8 text-center text-lg">
             <h1>Раздача Робуксов — получить Робуксы бесплатно в Роблоксе</h1>
+            <p class="animated-pulsating-text">Готовим вас к раздаче Робуксов...</p>
+            <x-countdown></x-countdown>
             <div class="flex flex-col py-10 px-8 text-left gap-4">
-                <x-input-label for="nickname" value="Имя пользователя" />
+                {{-- <x-input-label for="nickname" value="Имя пользователя" />
                 <x-text-input id="nickname" class="block mt-1 w-full" type="text" />
                 <x-input-label for="platform"  value="Платформа" />
-                <select id="platform"><option>Android</option><option>Windows</option><option>Xbox One</option><option>IOS</option><option>MacOS</option></select>
-                <x-input-label for="amount"  value="Сколько надо Robux?" />
+                <select id="platform"><option>Android</option><option>Windows</option><option>Xbox One</option><option>IOS</option><option>MacOS</option></select> --}}
+                <x-input-label for="amount"  value="Сколько бы Robux вы хотели получить?" />
                 <select id="amount"><option>500</option><option>1000</option><option>5000</option><option>10000</option></select>
             </div>
-            <x-countdown></x-countdown>
+            
             <div class="flex flex-col items-center">
                 <a href="{{ route('giveaway.quiz', ['step' => intval($step)+1]) }}"
                     class="block quiz-action-btn text-center max-w-96 my-4" style="display:none">Получить Robux бесплатно</a>
