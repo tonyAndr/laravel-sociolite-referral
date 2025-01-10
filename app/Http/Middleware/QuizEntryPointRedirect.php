5cd212501ddca97bf863bb539b69a9c6ac8a108b
TonyAndr <blueberry.acc@gmail.com>
2025-01-10 12:49:09 +0000

    Fix redirect again

--- app/Http/Middleware/QuizEntryPointRedirect.php
+++ app/Http/Middleware/QuizEntryPointRedirect.php
@@ -17,10 +17,19 @@ class QuizEntryPointRedirect
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $prevUrl = $request->session()->previousUrl();
-        $isSamePrevUrl = $prevUrl && (str_contains($prevUrl, 'tonyandr.com') || str_contains($prevUrl, 'luchbux.fun'));
+
+        $ua = $request->server('HTTP_USER_AGENT') ?? '';
         $fullPath = $request->fullUrl();
         $isQuizStep = str_contains($fullPath, '/giveaway/quiz?step=');
+        
+        // redirect Yandex bots
+        if (str_contains(strtolower($ua), 'yandex') && $isQuizStep) {
+            return redirect()->route('giveaway', [], 301);
+        }
+
+        // redirect users from other sites
+        $prevUrl = $request->session()->previousUrl() ?? '';
+        $isSamePrevUrl = $prevUrl && (str_contains($prevUrl, 'tonyandr.com') || str_contains($prevUrl, 'luchbux.fun'));
         if ($isQuizStep && !$isSamePrevUrl) {
             return redirect()->route('giveaway', [], 302);
         }
