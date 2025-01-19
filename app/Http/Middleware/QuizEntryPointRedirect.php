ce91f523637b8c6810f45d79d0707b1e293739ea
TonyAndr <blueberry.acc@gmail.com>
2025-01-19 12:19:09 +0000

    fix redirects

--- app/Http/Middleware/QuizEntryPointRedirect.php
+++ app/Http/Middleware/QuizEntryPointRedirect.php
@@ -21,18 +21,23 @@ class QuizEntryPointRedirect
         $ua = $request->server('HTTP_USER_AGENT') ?? '';
         $fullPath = $request->fullUrl();
         $isQuizStep = str_contains($fullPath, '/giveaway/quiz?step=');
-        
+        $referer = $request->headers->get('referer') ?? '';
+        $appUrl = env('APP_URL', 'luchbux.fun');
+        $parsedUrl = parse_url($appUrl);
+        $ourDomain = $parsedUrl['host'] ?? 'luchbux.fun';
+        $sameReferer = str_contains($referer, $ourDomain);
+
         // redirect Yandex bots
-        if (str_contains(strtolower($ua), 'yandex') && $isQuizStep) {
+        if (str_contains(strtolower($ua), 'yandex') && $isQuizStep && !$sameReferer) {
             return redirect()->route('giveaway', [], 301);
         }
 
         // redirect users from other sites
-        $prevUrl = $request->session()->previousUrl() ?? '';
-        $isSamePrevUrl = $prevUrl && (str_contains($prevUrl, 'tonyandr.com') || str_contains($prevUrl, 'luchbux.fun'));
-        if ($isQuizStep && !$isSamePrevUrl) {
-            return redirect()->route('giveaway', [], 302);
-        }
+        // $prevUrl = $request->session()->previousUrl() ?? '';
+        // $isSamePrevUrl = $prevUrl && (str_contains($prevUrl, 'tonyandr.com') || str_contains($prevUrl, 'luchbux.fun'));
+        // if ($isQuizStep && !$isSamePrevUrl) {
+        //     return redirect()->route('giveaway', [], 302);
+        // }
 
         return $next($request);
     }
